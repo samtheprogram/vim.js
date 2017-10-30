72dce013d6edec060aa1465e75f895be7de965d5
tinmarino <tinmarino@gmail.com>
2017-10-30 09:38:51 +0100

    Build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-set -e
-[ -z $EM_DIR] && EM_DIR=~/src/emscripten
-
 do_config() {
     echo config
 # something wrong with emcc + cproto, use gcc as CPP instead
 CPPFLAGS="-Os -DFEAT_GUI_WEB" \
 CPP="gcc -E" \
-$EM_DIR/emconfigure ./configure \
+# CPPCLAGS
+# LDFLAGS 
+# CC      -> /usr/share/emscripten/emcc
+
+/usr/bin/emconfigure ./configure \
     --enable-gui=web \
     --with-features=small \
     --disable-selinux \
@@ -40,20 +41,16 @@ $EM_DIR/emconfigure ./configure \
     --disable-gpm \
     --disable-sysmouse \
     --disable-nls \
-    --with-modified-by="Lu Wang" \
-    --with-compiledby="Lu Wang" \
-
+    --with-compiledby="Tinmarino" 
 }
 
 do_make() {
-$EM_DIR/emmake make -j8
+emmake make -j8
 }
 
 do_link() {
 pushd web
 cp ../src/vim vim.bc
-#cp vim_lib.js usr/local/share/vim/example.js
-cat vim_lib.js | sed -e "1 s/\(foldmethod\|foldmarker\)[^ ]\+//g" > usr/local/share/vim/example.js
 
 OPT_ASYNCIFY="-s ASYNCIFY=1 \
     -s EXPORTED_FUNCTIONS=\"['_main', '_input_available', '_gui_web_handle_key']\" \
@@ -63,11 +60,8 @@ OPT_EMTERPRETER="-s EMTERPRETIFY=1 -s EMTERPRETIFY_ASYNC=1"
 
 
 # Use vim.js as filename to generate vim.js.mem
-$EM_DIR/emcc vim.bc \
+emcc vim.bc \
     -o vim.js \
-    -Oz \
-    $OPT_EMTERPRETER \
-    --memory-init-file 1 \
     --js-library vim_lib.js \
     --embed-file usr \
 
