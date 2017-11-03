f36b9c9edba617cd8dad296f4a1814952ffab620
tinmarino <tinmarino@gmail.com>
2017-11-03 21:43:13 +0100

    vim_launch: add full screen support (not working) + load more for syntax (todo syntax load)

--- web/vim_launch.js
+++ web/vim_launch.js
@@ -17,6 +17,7 @@ function tin_load(path, file){
 
 
 
+
 function tin_load_doc(file){
 	tin_load("vim/doc", file);
 }
@@ -36,6 +37,8 @@ function tin_download(file, content){
     dl.click();
 }
 
+
+tin_full_screen();
 
 var Module;
 if(typeof Module==="undefined")Module=eval("(function() { try { return Module || {} } catch(e) { return {} } })()");
@@ -70,31 +73,29 @@ Module["FS_createPath"]("/vim/cheatsheet","doc",true,true);
 // Yu heritage
 tin_load("vim", "vimrc");
 Module["FS_createLink"]("/", ".vimrc", "/vim/vimrc", true, true);
-tin_load("vim/syntax","synload.vim");
-tin_load("vim/syntax","syntax.vim");
-tin_load("vim/syntax","javascript.vim");
-tin_load("vim/syntax","nosyntax.vim");
-tin_load("vim/syntax","vim.vim");
 tin_load("vim/colors","Darkside.vim");
 
-// Tin heritage
 tin_load("vim", "first-page.txt");
 tin_load("vim", "goat1.txt");
+// Syntax
+tin_load("vim/syntax", "syntax.vim");
+tin_load("vim/syntax", "nosyntax.vim");
+tin_load("vim/syntax", "synload.vim");
 tin_load("vim/syntax", "myhelp.vim");
 tin_load("vim/syntax", "help.vim");
+tin_load("vim/syntax", "vim.vim");
 tin_load("vim/syntax", "sh.vim");
 tin_load("vim/syntax", "python.vim");
-// <-- Tinmarino 
 
 // Load AnsiEsc
-Module["FS_createPath"]("/vim", "plugin", true, true);
-Module["FS_createPath"]("/vim/plugin", "ansiesc", true, true);
-Module["FS_createPath"]("/vim/plugin/ansiesc", "autoload", true, true);
-Module["FS_createPath"]("/vim/plugin/ansiesc", "plugin", true, true);
-
-tin_load("vim/plugin/ansiesc/autoload", "AnsiEsc.vim");
-tin_load("vim/plugin/ansiesc/plugin", "AnsiEscPlugin.vim");
-tin_load("vim/plugin/ansiesc/plugin", "cecutil.vim");
+// Module["FS_createPath"]("/vim", "plugin", true, true);
+// Module["FS_createPath"]("/vim/plugin", "ansiesc", true, true);
+// Module["FS_createPath"]("/vim/plugin/ansiesc", "autoload", true, true);
+// Module["FS_createPath"]("/vim/plugin/ansiesc", "plugin", true, true);
+// 
+// tin_load("vim/plugin/ansiesc/autoload", "AnsiEsc.vim");
+// tin_load("vim/plugin/ansiesc/plugin", "AnsiEscPlugin.vim");
+// tin_load("vim/plugin/ansiesc/plugin", "cecutil.vim");
 
 
 // End from Yu
@@ -107,3 +108,17 @@ Module["preRun"].push(runWithFS)}}))();
 
 // Include the baby 
 document.write('<script type="text/javascript" src="vim.js" ></script>')
+
+
+
+function tin_full_screen(){
+    var wdemo = window.innerWidth - 6;
+    var hdemo = window.innerHeight - 6;
+    var wold = document.getElementById("vimjs-container").clientWidth;
+    var hold = document.getElementById("vimjs-container").clientHeight;
+    console.log("tinp: set div size: " + wdemo + ", " + hdemo + " from " + wold + ", " + hold);
+    document.getElementById("vimjs-canvas").width = wdemo +  "px";
+    document.getElementById("vimjs-canvas").height = hdemo +  "px";
+    document.getElementById("vimjs-container").style.width = wdemo +  "px";
+    document.getElementById("vimjs-container").style.height = hdemo +  "px";
+}
