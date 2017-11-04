85f2133817609401cf4b8b144d04b39e9d9b6f5c
tinmarino <tinmarino@gmail.com>
2017-11-04 16:26:03 +0100

    Help files from github Vim main repo

--- web/vim_launch.js
+++ web/vim_launch.js
@@ -1,22 +1,61 @@
 
 
-function tin_load(path, file){
-	// path:  "vim/doc"
-	// file:  "v-tips.txt" 
-	console.log("tin: " + path + "/" + file);
+function tin_load(path){
+	console.log("load: " + path);
+	var dir = path.split("/");
+	dir.pop();
+	var dir = dir.join("/");
+	var file = path.split("/").slice(-1)[0];
+
+	// vimdoc from github
+	if (dir == "/vim/txt/vimdoc"){
+		if (!file.endsWith(".txt")){
+			file += ".txt";
+		}
+		var url = "https://raw.githubusercontent.com/vim/vim/master/runtime/doc"
+	 	url += "/" + file
+		tin_url(path, url);
+	}
+	// rest here
+	else{
+		tin_local(dir, file);
+	}
+}
+
+
+function tin_local(path, file){
+	path = path.substr(1);
+	console.log("local: " + path + ", " + file );
 	var xhttp = new XMLHttpRequest();
 	xhttp.open("GET", path + "/" + file, false);
     xhttp.overrideMimeType('text/xml; charset=x-user-defined');
 	xhttp.send();
 	var res = xhttp.responseText;
-	Module["FS_createDataFile"]( "/" + path, file, res, true, true);
+	Module["FS_createDataFile"]( path, file, res, true, true);
 }
 
-function tin_load_doc(file){
-	tin_load("vim/doc", file);
+function tin_url(path, url){
+	// path (_out_): /vim/txt/vimdoc/help.txt"
+	// url  (_in_) : https://raw.githubusercontent.com/vim/vim/master/runtime/doc/help.txt
+	// Request
+	var xhttp = new XMLHttpRequest();
+	xhttp.open("GET", url, false);
+	xhttp.send();
+	var res = xhttp.responseText;
+	// Path
+	var dir = path.split("/");
+	dir.pop();
+	var dir = dir.join("/");
+	// File
+	var file = path.split("/").slice(-1)[0];
+	// Save
+	console.log("load_file: " + dir + ", " + file + " from " + url);
+	Module["FS_createDataFile"](dir, file, res, true, true);
 }
+
 
 function tin_href(link){
+	// link: to be opened
     window.open(link, '<-vim.js');
 }
 
@@ -66,27 +105,27 @@ Module["FS_createPath"]("/vim/txt", "tindoc", true, true);
 Module["FS_createPath"]("/vim/txt", "cheatdoc", true, true);
 
 
-tin_load("vim", "first-page.txt");
-tin_load("vim", "goat1.txt");
+tin_load("/vim/first-page.txt");
+tin_load("/vim/goat1.txt");
 
 // Vimrc
-tin_load("vim", "vimrc");
+tin_load("/vim/vimrc");
 
 // Syntax
-tin_load("vim/colors","Darkside.vim");
-tin_load("vim/syntax", "syntax.vim");
-tin_load("vim/syntax", "nosyntax.vim");
-tin_load("vim/syntax", "synload.vim");
-tin_load("vim/syntax", "myhelp.vim");
-tin_load("vim/syntax", "help.vim");
-tin_load("vim/syntax", "vim.vim");
-tin_load("vim/syntax", "sh.vim");
-tin_load("vim/syntax", "python.vim");
+tin_load("/vim/colors/Darkside.vim");
+tin_load("/vim/syntax/syntax.vim");
+tin_load("/vim/syntax/nosyntax.vim");
+tin_load("/vim/syntax/synload.vim");
+tin_load("/vim/syntax/myhelp.vim");
+tin_load("/vim/syntax/help.vim");
+tin_load("/vim/syntax/vim.vim");
+tin_load("/vim/syntax/sh.vim");
+tin_load("/vim/syntax/python.vim");
 
 // Index doc
-tin_load("vim/txt/vimdoc", "help.txt");
-tin_load("vim/txt/tindoc", "meta-help.txt");
-tin_load("vim/txt/cheatdoc", "c-meta.txt");
+tin_url("/vim/txt/vimdoc/help.txt", "https://raw.githubusercontent.com/vim/vim/master/runtime/doc/help.txt");
+tin_load("/vim/txt/tindoc/meta-help.txt");
+tin_load("/vim/txt/cheatdoc/c-meta.txt");
 
 // Load AnsiEsc
 // Module["FS_createPath"]("/vim", "plugin", true, true);
@@ -105,10 +144,10 @@ Module["preRun"].push(runWithFS)}}))();
 
 
 
-tin_full_screen();
 
 
 // Include the baby 
 document.write('<script type="text/javascript" src="vim.js" ></script>')
 
 
+tin_full_screen();
