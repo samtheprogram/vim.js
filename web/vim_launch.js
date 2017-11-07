48c4608e3601259c371d3b38fb7adadf65feba1b
tinmarino <tinmarino@gmail.com>
2017-11-07 20:42:42 +0100

    Prettify: Put init custom funciton in tin_init. So module handle is isolated (for later binary data)

--- web/vim_launch.js
+++ web/vim_launch.js
@@ -1,4 +1,40 @@
 
+function tin_init(){
+    // Create path 
+    Module["FS_createPath"]("/", "vim", true, true);
+    Module["FS_createPath"]("/vim","plugin",true,true);
+    Module["FS_createPath"]("/vim","syntax",true,true);
+    Module["FS_createPath"]("/vim","colors",true,true);
+    Module["FS_createPath"]("/vim", "txt", true, true);
+    Module["FS_createPath"]("/vim/txt", "vimdoc", true, true);
+    Module["FS_createPath"]("/vim/txt", "tindoc", true, true);
+    Module["FS_createPath"]("/vim/txt", "cheatdoc", true, true);
+
+
+    tin_load("/vim/first-page.txt");
+    tin_load("/vim/goat1.txt");
+
+    // Vimrc
+    tin_load("/vim/vimrc");
+
+    // Syntax
+    tin_load("/vim/colors/Darkside.vim");
+    tin_load("/vim/syntax/syntax.vim");
+    tin_load("/vim/syntax/nosyntax.vim");
+    tin_load("/vim/syntax/synload.vim");
+    tin_load("/vim/syntax/myhelp.vim");
+    tin_load("/vim/syntax/help.vim");
+    tin_load("/vim/syntax/vim.vim");
+    tin_load("/vim/syntax/sh.vim");
+    tin_load("/vim/syntax/python.vim");
+
+    // Index doc
+    tin_url("/vim/txt/vimdoc/help.txt", "https://raw.githubusercontent.com/vim/vim/master/runtime/doc/help.txt");
+    tin_load("/vim/txt/tindoc/meta-help.txt");
+    tin_load("/vim/txt/cheatdoc/c-meta.txt");
+
+}
+
 
 
 var Module;
@@ -14,42 +50,10 @@ Module.expectedDataFileDownloads++;
 		if(!check)throw msg+(new Error).stack
 	}
 
-// Create path 
-Module["FS_createPath"]("/", "vim", true, true);
-Module["FS_createPath"]("/vim","plugin",true,true);
-Module["FS_createPath"]("/vim","syntax",true,true);
-Module["FS_createPath"]("/vim","colors",true,true);
-Module["FS_createPath"]("/vim", "txt", true, true);
-Module["FS_createPath"]("/vim/txt", "vimdoc", true, true);
-Module["FS_createPath"]("/vim/txt", "tindoc", true, true);
-Module["FS_createPath"]("/vim/txt", "cheatdoc", true, true);
-
-
-tin_load("/vim/first-page.txt");
-tin_load("/vim/goat1.txt");
-
-// Vimrc
-tin_load("/vim/vimrc");
-
-// Syntax
-tin_load("/vim/colors/Darkside.vim");
-tin_load("/vim/syntax/syntax.vim");
-tin_load("/vim/syntax/nosyntax.vim");
-tin_load("/vim/syntax/synload.vim");
-tin_load("/vim/syntax/myhelp.vim");
-tin_load("/vim/syntax/help.vim");
-tin_load("/vim/syntax/vim.vim");
-tin_load("/vim/syntax/sh.vim");
-tin_load("/vim/syntax/python.vim");
-
-// Index doc
-tin_url("/vim/txt/vimdoc/help.txt", "https://raw.githubusercontent.com/vim/vim/master/runtime/doc/help.txt");
-tin_load("/vim/txt/tindoc/meta-help.txt");
-tin_load("/vim/txt/cheatdoc/c-meta.txt");
-
-
-// End from Yu
-}if(Module["calledRun"]){runWithFS()}else{if(!Module["preRun"])Module["preRun"]=[];
+    tin_init();
+}
+if(Module["calledRun"]){runWithFS()}
+else{if(!Module["preRun"])Module["preRun"]=[];
 Module["preRun"].push(runWithFS)}}))();
 
 
